fd60aa70d9191a20a00020553fc03f8e9830cdee
rhxu <rhxu@umich.edu>
2023-05-15 14:57:53 -0400

    2d particle filter seems working

--- 2d_particle_filter.md
+++ 2d_particle_filter.md
@@ -6,4 +6,12 @@
 This simulation is inspried by this [tutorial](https://salzi.blog/2015/05/25/particle-filters-with-python/). A Robot is in a 2D map with some obstacle in it, the robot knows its distance to every obstacles, and the aim is to locolize the robot in the map. In this setting, we are not considring the orientation of the robot, instead we are assuming the robot has Omiwheels
 ![2d_simple_case](2d_simple_case.png)
 
-### Method
+### Method / process outline 
+1. random sample on the whole map as initial particles 
+2. apply filter
+   1. compare each particle with the current measurement 
+   2. if current particle looks like a good guess, increase its weight
+   3. after process every particle, do a resampling 
+   4. take the highly weight particles, and generate new samples around those particles
+3. Advance time step, moving the plane and the particles by the control (3m/s)
+4. repeat 2 and 3 until find good estimation
--- 2d_pf.py
+++ 2d_pf.py
@@ -15,13 +15,14 @@ def measurement(x, y):
     distance = [0] * OBSTACLE_NUM
     for i in range(OBSTACLE_NUM):
         disx = x - obstacleX[i]
-        disy = x - obstacleY[i]
+        disy = y - obstacleY[i]
         dis = np.sqrt(disx**2 + disy**2)
-        distance[i] = dis[0]
+        distance[i] = dis
     return distance
 
 
 def particle_dis():
+    # an array that store the distance to each obstacle
     particle_distances = []
     for particle in particles:
         particle_distances.append(measurement(particle[0], particle[1]))
@@ -31,6 +32,8 @@ def particle_dis():
 def create_weight(particles, particle_distances, measurement):
     # if the particle distance matches the mesurement (within certian error)
     # it means it is a good guess so increase the weight
+    # if the particle is not within map boundry, we set the weight to be 0
+    # which means it has no chance to be picked for resampling 
     weight = [0] * SMAPLE_NUM
     # if a particle's distance to each obstacle has a samller difference compare to the measurement, assign higher weight
     for i in range(len(particles)):
@@ -63,44 +66,71 @@ def sample_from_accu_weight(accu_weight, particles):
     position = step
     for i in range(SMAPLE_NUM):
         index = next(x for x, val in enumerate(accu_weight) if val >= position)
-        new_particle = [particles[index][0] + np.random.random_sample() /
-                        5 - 0.1, particles[index][1] + np.random.random_sample()/5 - 0.1]
+        # random sample around the original particle
+        new_particle = [particles[index - 1][0] + (np.random.random_sample(
+        ) - 0.5)/5, particles[index - 1][1] + (np.random.random_sample() - 0.5)/5]
         new_particles.append(new_particle)
+        position += step
     return new_particles
 
 
+def move_particles(dx, dy, particles):
+    for particle in particles:
+        particle[0] += dx
+        particle[1] += dy
+    return particles
+
+
 def onclick(event):
+    global particles, particle_points
     print("key=" + str(event.key))
     x = point.get_xdata()
     y = point.get_ydata()
     # measurement
     distance = measurement(point.get_xdata(), point.get_ydata())
+    # the distance of each particle to every obstacle
     particle_distances = particle_dis()
-    print(distance)
+    # create the wegit for each particle according to the measurment
+    weight = create_weight(particles, particle_distances, distance)
+    # create accumulative weight for uniform sampling
+    accu_weight = create_accumulative_weight(weight)
+    # generate new particles to replace the old once
+    particles = sample_from_accu_weight(accu_weight, particles)
+
+    # print(distance)
+    # process key board input to move the robot and particles
     if (event.key == 'left'):
         point.set_data(x - 0.1, y)
+        particles = move_particles(-0.1, 0, particles)
     if (event.key == 'right'):
         point.set_data(x + 0.1, y)
+        particles = move_particles(0.1, 0, particles)
     if (event.key == 'up'):
         point.set_data(x, y + 0.1)
+        particles = move_particles(0, 0.1, particles)
     if (event.key == 'down'):
         point.set_data(x, y - 0.1)
+        particles = move_particles(0, -0.1, particles)
+    # update the particle position in the plot
+    for i in range(len(particle_points)):
+        particle_points[i].set_data(particles[i][0], particles[i][1])
 
     # fig.canvas.draw()
     plt.draw()
 
 
-point, = ax.plot(5, 5, 'or')
+point, = ax.plot(5, 5, 'or', zorder=10)
 obstacleX = np.random.random((OBSTACLE_NUM,)) * 10
 obstacleY = np.random.random((OBSTACLE_NUM,)) * 10
-particles = []
-particle_points = []
+particles = []  # sotre (x,y) pair of the position of the particle
+particle_points = []  # an instance of the ploted particle
 for i in range(SMAPLE_NUM):
     particle = [np.random.random(1)[0] * 10, np.random.random(1)[0] * 10]
-    particle_points.append(ax.plot(particle[0], particle[1], 'oy'))
+    p_point, = ax.plot(particle[0], particle[1], 'oy', zorder=5)
+    particle_points.append(p_point)
     particles.append(particle)
 
 print(particles)
-ax.plot(obstacleX, obstacleY, 'ob')
+ax.plot(obstacleX, obstacleY, 'ob', zorder=7)
 cid = fig.canvas.mpl_connect('key_press_event', onclick)
 plt.show()
